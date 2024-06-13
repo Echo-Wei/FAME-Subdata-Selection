3d5c42244e63cf359fc431185f26933ff70457bb
Echo-Wei <65644671+Echo-Wei@users.noreply.github.com>
2024-06-13 01:02:23 -1000

    Add Sample Dataset files.

---
+++ Dataset/Simulated_Independent_Dataset.R
@@ -0,0 +1,18 @@
+# Generate Independent Data Normal --------------------------------------------------------------------
+library(MASS)
+set.seed(12300)
+n = 2000; p = 1000; rho = 0.5
+mu <- rnorm(p, 0, 10)           
+sigma <- diag(rep(1, p))
+
+# Set the first five coefficients as true coefficients, others as 0
+beta <- rep(0, p)
+tr <- 5
+index_beta <- c(1:tr)
+beta[index_beta] <- rnorm(tr, 0, 10)            # Sampling beta with 5 non-zero coefficients
+
+# Generate the response variable and predictor variables
+x = mvrnorm(n, mu, sigma)      
+error = rnorm(n, 0, 4)                       
+y = x%*%beta + error            
+data = cbind(y, x)
