fe96aa5a6e919df739f50e44ee828fa3a26c5439
Echo-Wei <65644671+Echo-Wei@users.noreply.github.com>
2024-07-29 23:33:41 -0700

    Update Simulated_Dependent_Dataset.R

--- Dataset/Simulated_Dependent_Dataset.R
+++ Dataset/Simulated_Dependent_Dataset.R
@@ -2,14 +2,14 @@
 library(MASS)
 set.seed(12300)
 n = 2000; p = 1000; rho = 0.5
-mu <- rnorm(p, 0, 10)            # Mean
+mu <- rnorm(p, 0, 4)            # Mean
 sigma <- diag(rep(1-rho, p)) + matrix(rho, p, p)        
 
 # Set the first five coefficients as true coefficients, others as 0
 beta <- rep(0, p)
 tr <- 5
 index_beta <- c(1:tr)
-beta[index_beta] <- rnorm(tr, 0, 10)            # Sampling beta with 5 non-zero coefficients
+beta[index_beta] <- abs(rnorm(tr, 0, 10))            # Sampling beta with 5 non-zero coefficients
 
 # Generate the response variable and predictor variables
 x = mvrnorm(n, mu, sigma)      
